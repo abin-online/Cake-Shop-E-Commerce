4622d467589b2c5b77f3ccab3ec1b208ebd6a5a5
Abin Babu <abinbabuonline@gmail.com>
2024-06-26 10:53:59 +0530

    glogin

--- middleware/googleAuth.js
+++ middleware/googleAuth.js
@@ -3,7 +3,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL:  "http://localhost:3333/auth/google/callback",
+    callbackURL:  "https://cakekart.shop/auth/google/callback",
     passReqToCallback: true
 },
     async function (request, accessToken, refreshToken, profile, done) {
