0e700d9119bd9af3272f626ad9c495a2d9ff6765
Abin Babu <abinbabuonline@gmail.com>
2024-06-26 09:35:58 +0530

    testing commit

--- controller/user/profile.js
+++ controller/user/profile.js
@@ -77,7 +77,6 @@ module.exports = {
             await address.save();
             res.redirect('/checkout');
         } catch (error) {
-            console.log(error);
             res.status(500).send('Internal Server Error');
         }
     },
